25d0f8ef82ab89c8ddc91cede9d21faf24ebeab1
CommanderRedYT <ccommanderred@gmail.com>
2023-08-27 21:36:16 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import http from 'http';
+import request from 'request';
+
+const proxy_for = process.env['PROXY_URL'];
+
+// http server that proxies requests to the proxy server and authenticates with digest auth
+
+http.createServer((req, res) => {
+    const options = {
+        url: `http://${proxy_for}${req.url}`,
+        auth: {
+            user: process.env['PROXY_USER'],
+            pass: process.env['PROXY_PASS'],
+            sendImmediately: false,
+        }
+    };
+    req.pipe(request(options)).pipe(res);
+}).listen(process.env['HTTP_PORT'] || 8080);
